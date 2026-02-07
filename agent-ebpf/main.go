6c7343cf150175490e341eb64afbf879f35f49ff
Louis PROTON <louis.proton@epitech.eu>
2026-02-07 22:45:59 +0100

    hotfix+0201

--- agent-ebpf/main.go
+++ agent-ebpf/main.go
@@ -7,10 +7,7 @@ import (
 	"net/http"
 	"os"
 	"os/exec"
-	"strconv"
-	"strings"
 	"sync"
-	"time"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
